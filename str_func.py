ce8b2a5662828a814a5762133824ed08a65585e9
Ayamileto <133178977+Ayamileto@users.noreply.github.com>
2023-05-20 01:52:03 +0300

    Update str_func.py
    correct dockstring at first func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,4 @@
 def upper_str(string):
-    """ Функция возвращает полученную строку в верхнем регистре """
+    """ Функция возвращает в верхнем регистре """
     upper_string = string.upper()
     return upper_string
