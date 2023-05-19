8f786b86aae7949b90bdbd519b7f1bcf5449ba32
Ayamileto <133178977+Ayamileto@users.noreply.github.com>
2023-05-20 00:34:57 +0300

    Update str_func.py
     add dickstring

--- str_func.py
+++ str_func.py
@@ -1,4 +1,4 @@
 def upper_str(string):
-    """ Функция возвращает строку в верхнем регистре """
+    """ Функция возвращает полученную строку в верхнем регистре """
     upper_string = string.upper()
     return upper_string
